6fab72e9fcf6b56ff963b1fc1a99247ba543ed92
Vusi <vnwedo@gmail.com>
2025-12-11 22:56:36 +0200

    get books based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,24 +10,45 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
 public_users.get('/',function (req, res) {
+  
   return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
+
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
   
+  const isbn = req.params.isbn; 
   if (books[isbn]) {
       return res.status(200).json(books[isbn]);
   } else {
       return res.status(404).json({message: `Book with ISBN ${isbn} not found.`});
   }
-});
+ });
   
 
 public_users.get('/author/:author',function (req, res) {
+  const author = req.params.author; 
+  let matchingBooks = {};
   
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  const bookKeys = Object.keys(books);
+
+  
+  for (const isbn of bookKeys) {
+      if (books[isbn].author === author) {
+          matchingBooks[isbn] = books[isbn];
+      }
+  }
+
+  if (Object.keys(matchingBooks).length > 0) {
+      
+      return res.status(200).json(matchingBooks);
+  } else {
+      
+      return res.status(404).json({message: `No books found by author: ${author}.`});
+  }
 });
 
 
